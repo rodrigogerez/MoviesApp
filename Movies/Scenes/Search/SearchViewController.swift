d805500bfc5630b5d64e8940bd20e39018b48d57
Rodrigo Gerez <rodrigoa.gerez@gmail.com>
2020-01-09 17:31:45 -0300

    Replaced Navigation Controller for Tab Bar Controller. Added search and category view controller.

---
+++ Movies/Scenes/Search/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Movies
+//
+//  Created by Rodrigo Gerez on 09/01/2020.
+//  Copyright © 2020 Rodrigo Gerez. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
